e81ad50d078253de56d3e751e1ee37eab37373fb
Arash Golgol <arash.golgol@gmail.com>
2024-03-29 11:30:50 +0000

    Report irq counter in sysfs

--- recipes-kernel/irq-example/files/irq-counter.c
+++ recipes-kernel/irq-example/files/irq-counter.c
@@ -16,19 +16,37 @@ MODULE_AUTHOR("Arash Golgol <arash.golgol@gmail.com>");
 MODULE_DESCRIPTION("Simple GPIO interrupt counter ");
 MODULE_VERSION("1.0.0");
 
+#define DEVICE_NAME "irq-cnt"
+#define CLASS_NAME "irq-cnt-class"
 #define GPIO_PE0 128
 //This used for storing the IRQ number for the GPIO
 unsigned int GPIO_irqNumber;
+unsigned int irq_cnt;
+static struct device *irq_device;
+static struct class *irq_class;
+static dev_t irq_dev;
+
+// debugging only
+static ssize_t irq_cnt_show(struct device *dev, struct device_attribute *attr, char *buf)
+{
+    return sprintf(buf, "%d", irq_cnt);
+}
+static DEVICE_ATTR(irq_cnt, S_IRUSR, irq_cnt_show, NULL);
 
 //Interrupt handler for GPIO. 
 //This will be called whenever there is a raising edge detected. 
 static irqreturn_t gpio_irq_handler(int irq,void *dev_id) 
 {
-  pr_info("Interrupt Occurred\n");
+  irq_cnt++;
   return IRQ_HANDLED;
 }
 
 static int __init irq_driver_init(void) {
+
+    int rval;
+    // cleanup counter
+    irq_cnt = 0;
+
     //Input GPIO configuratioin
     //Checking the GPIO is valid or not
     if(gpio_is_valid(GPIO_PE0) == false){
@@ -58,9 +76,42 @@ static int __init irq_driver_init(void) {
         goto r_gpio_in;
     }
   
+
+/* Alloc a device region */
+    rval = alloc_chrdev_region(&irq_dev, 1, 1, DEVICE_NAME);
+    if (rval != 0)          /* error */
+       goto cdev_alloc_err;
+
+/* class */
+    irq_class = class_create(CLASS_NAME);
+    if (IS_ERR(irq_class)) {
+        printk(KERN_ERR DEVICE_NAME " cant create class %s\n", CLASS_NAME);
+        goto class_err;
+    }
+
+/* device */
+    irq_device = device_create(irq_class, NULL, irq_dev, NULL, DEVICE_NAME);
+    if (IS_ERR(irq_device)) {
+        printk(KERN_ERR DEVICE_NAME " cant create device %s\n", DEVICE_NAME);
+        goto device_err;
+     }
+
+/* device attribute on sysfs */
+    rval = device_create_file(irq_device, &dev_attr_irq_cnt);
+    if (rval < 0) {
+        printk(KERN_ERR DEVICE_NAME " cant create device attribute %s %s\n", 
+        DEVICE_NAME, dev_attr_irq_cnt.attr.name);
+    }
+
     pr_info("Device Driver Insert...Done!!!\n");
     return 0;
 
+device_err:
+    class_unregister(irq_class);
+    class_destroy(irq_class);
+cdev_alloc_err:
+class_err:
+    free_irq(GPIO_irqNumber, NULL);
 r_gpio_in:
     gpio_free(GPIO_PE0);
     return -1;
@@ -71,6 +122,10 @@ r_gpio_in:
 */
 static void __exit irq_driver_exit(void)
 {
+    unregister_chrdev_region(irq_dev, 1);
+    device_destroy(irq_class, irq_dev);
+    class_unregister(irq_class);
+    class_destroy(irq_class);
     free_irq(GPIO_irqNumber,NULL);
     gpio_free(GPIO_PE0);
     pr_info("Device Driver Remove...Done!!\n");
